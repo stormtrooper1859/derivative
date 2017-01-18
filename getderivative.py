c5fe812787b0491100a722b725266ca085646bc9
Alex Osipov <sashaosipov.98@gmail.com>
2017-01-18 16:31:40 +0300

    given to the PEP-8 and remove dependency on gaps

--- getderivative.py
+++ getderivative.py
@@ -1,28 +1,48 @@
 import sys
 
 function_name = ["ln", "sin", "cos", "tg", "ctg", "arcsin", "arctg"]
-priority = [['(', ')'], ['+', '-'], ['*', '/'], ["**"]]
+priority = [['+', '-'], ['*', '/'], ["**"], ['(', ')']]
 right_associative = ["**"]
 max_priority_val = len(priority)
 
-def getPriority(str):
+ml = ["ln", "sin", "cos", "tg", "ctg", "arcsin", "arctg", '(', ')', '+', '-', '**', '/', "*"]
+
+
+def to_list(in_str):
+    if in_str == '':
+        return list()
+    for prt in ml:
+        tmp = in_str.partition(prt)
+        if tmp[1] != '':
+            answer = list()
+            answer.extend(to_list(tmp[0]))
+            answer.append(tmp[1])
+            answer.extend(to_list(tmp[2]))
+            return answer
+    return list(in_str)
+
+
+def get_priority(in_str):
     for i in range(0, max_priority_val):
         for t in priority[i]:
-            if(str == t):
+            if in_str == t:
                 return i
     return -1
 
-def isRightAssociative(str):
+
+def is_right_associative(in_str):
     for t in right_associative:
-        if(str == t):
+        if in_str == t:
             return True
     return False
 
-def isFunction(str):
+
+def is_function(in_str):
     for t in function_name:
-        if (str == t):
+        if in_str == t:
             return True
     return False
+
 
 def is_number(s):
     try:
@@ -31,161 +51,135 @@ def is_number(s):
     except ValueError:
         return False
 
+
 def main(argv):
     infile = open(argv[1], 'r')
     outfile = open(argv[2], 'w')
-    instrstr = infile.readline()
-    while(instrstr != ""):
-        instr = instrstr.split()
-        instrstr = infile.readline()
-        outstr = list()
-        operationstack = list()
-        #toreversePolishNotation
-        for elem in instr:
-            while (elem[0] == '('):
-                elem = elem[1:]
-                operationstack.append('(')
-            flag = True
-            while(flag):
-                flag = False
-                flagName = ""
-                for t1 in function_name:
-                    if (elem.find(t1) == 0):
-                        flag = True
-                        flagName = t1
-                if(flag):
-                    operationstack.append(flagName)
-                    elem.replace(flagName, "")
-                    elem = elem[(len(flagName)):]
-                while (elem[0] == '('):
-                    elem = elem[1:]
-                    operationstack.append('(')
-            closebracket_count = 0
-            while(elem[0] == '('):
-                elem = elem[1:]
-                operationstack.append('(')
-            while(elem[len(elem) - 1] == ')'):
-                elem = elem[:len(elem) - 1]
-                closebracket_count += 1
-            if(is_number(elem)):
-                outstr.append(elem)
-            elif(elem == 'x' or elem == 'e'):
-                outstr.append(elem)
+    instr = infile.readline().strip()
+    while instr != "":
+        in_list = instr.replace(' ', '')
+        in_list = to_list(in_list)
+        instr = infile.readline().strip()
+        rpn_list = list()
+        operation_stack = list()
+
+        # to reverse Polish Notation
+        for elem in in_list:
+            if elem == '(':
+                operation_stack.append('(')
+            elif elem == ')':
+                temp = operation_stack.pop()
+                while temp != '(':
+                    rpn_list.append(temp)
+                    temp = operation_stack.pop()
+                if len(operation_stack) != 0:
+                    temp3 = operation_stack.pop()
+                    if is_function(temp3):
+                        rpn_list.append(temp3)
+                    else:
+                        operation_stack.append(temp3)
+            elif is_number(elem):
+                rpn_list.append(elem)
+            elif elem == 'x' or elem == 'e':
+                rpn_list.append(elem)
+            elif is_function(elem):
+                operation_stack.append(elem)
             else:
-                if(len(operationstack) != 0):
-                    temp = operationstack.pop()
-                    while((temp != "none") and (isRightAssociative(temp) and (getPriority(elem) < getPriority(temp)))
-                          or (not isRightAssociative(str) and (getPriority(elem) <= getPriority(temp)))):
-                        outstr.append(temp)
-                        if(len(operationstack) == 0):
-                            temp = "none"
+                if len(operation_stack) != 0:
+                    temp2 = operation_stack.pop()
+                    while((temp2 != 'none' and temp2 != '(')
+                          and ((is_right_associative(temp2) and (get_priority(elem) < get_priority(temp2)))
+                               or (not is_right_associative(temp2) and (get_priority(elem) <= get_priority(temp2))))):
+                        rpn_list.append(temp2)
+                        if len(operation_stack) == 0:
+                            temp2 = 'none'
                         else:
-                            temp = operationstack.pop()
-                    if(temp != "none"):
-                        operationstack.append(temp)
-                operationstack.append(elem)
-            for i in range(0, closebracket_count):
-                temp = operationstack.pop()
-                while (temp != '('):
-                    outstr.append(temp)
-                    if(len(operationstack) == 0):
-                        temp = '('
-                    else:
-                        temp = operationstack.pop()
-                        if(isFunction(temp)):
-                            outstr.append(temp)
-                            if (len(operationstack) == 0):
-                                temp = '('
-                            else:
-                                temp = operationstack.pop()
-                if(len(operationstack) != 0):
-                    temp3 = operationstack.pop()
-                    if(isFunction(temp3)):
-                        outstr.append(temp3)
-                    else:
-                        operationstack.append(temp3)
-        while(len(operationstack) != 0):
-            outstr.append(operationstack.pop())
+                            temp2 = operation_stack.pop()
+                    if temp2 != "none":
+                        operation_stack.append(temp2)
+                operation_stack.append(elem)
+            # print(rpn_list)
+            # print(operation_stack)
+            # print("-" * 5)
+        while len(operation_stack) != 0:
+            rpn_list.append(operation_stack.pop())
 
-        #print(outstr)
+        # print(rpn_list)
 
-        #getDerivative
-        workStack = list()
+        # get Derivative
+        work_stack = list()
         expressions = list()
         derivatives = list()
         expr = ""
         der = ""
-        for elem in outstr:
-            if(is_number(elem) or elem == 'e'):
+        for elem in rpn_list:
+            if is_number(elem) or elem == 'e':
                 expressions.append(elem)
                 derivatives.append("0")
-                workStack.append(len(expressions) - 1)
-            elif(elem == 'x'):
+                work_stack.append(len(expressions) - 1)
+            elif elem == 'x':
                 expressions.append(elem)
                 derivatives.append("1")
-                workStack.append(len(expressions) - 1)
+                work_stack.append(len(expressions) - 1)
             else:
-                if(isFunction(elem)):
-                    temp1 = workStack.pop()
-                    if(elem == "ln"):
+                if is_function(elem):
+                    temp1 = work_stack.pop()
+                    if elem == "ln":
                         expr = "ln(" + expressions[temp1] + ")"
                         der = "(1 / " + expressions[temp1] + ") * " + derivatives[temp1]
-                    elif(elem == "sin"):
+                    elif elem == "sin":
                         expr = "sin(" + expressions[temp1] + ")"
                         der = "cos(" + expressions[temp1] + ") * " + derivatives[temp1]
-                    elif (elem == "cos"):
+                    elif elem == "cos":
                         expr = "cos(" + expressions[temp1] + ")"
                         der = "-sin(" + expressions[temp1] + ") * " + derivatives[temp1]
-                    elif (elem == "tg"):
+                    elif elem == "tg":
                         expr = "tg(" + expressions[temp1] + ")"
                         der = "(1 / cos(" + expressions[temp1] + ") ** 2) * " + derivatives[temp1]
-                    elif (elem == "ctg"):
+                    elif elem == "ctg":
                         expr = "ctg(" + expressions[temp1] + ")"
                         der = "(-1 / sin(" + expressions[temp1] + ") ** 2) * " + derivatives[temp1]
-                    elif (elem == "arcsin"):
+                    elif elem == "arcsin":
                         expr = "arcsin(" + expressions[temp1] + ")"
                         der = "(1 / (1 - " + expressions[temp1] + " ** 2) ** (1 / 2)) * " + derivatives[temp1]
-                    elif (elem == "arctg"):
+                    elif elem == "arctg":
                         expr = "arcsin(" + expressions[temp1] + ")"
                         der = "(1 / 1 + " + expressions[temp1] + " ** 2) * " + derivatives[temp1]
                 else:
-                    temp2 = workStack.pop()
-                    temp1 = workStack.pop()
-                    if(elem == '+' or elem == '-'):
+                    temp2 = work_stack.pop()
+                    temp1 = work_stack.pop()
+                    if elem == '+' or elem == '-':
                         expr = "(" + expressions[temp1] + " " + elem + " " + expressions[temp2] + ")"
                         der = "(" + derivatives[temp1] + " " + elem + " " + derivatives[temp2] + ")"
-                    elif(elem == '*'):
-                        expr = "(" + expressions[temp1] + " * " + expressions[temp2] + ")" # ?
-                        der = "(" + derivatives[temp1] + " * " + expressions[temp2] + " + " + expressions[temp1] + " * " + derivatives[temp2] + ")"
-                    elif(elem == '/'):
-                        if(expressions[temp1] == '0'):
+                    elif elem == '*':
+                        expr = "(" + expressions[temp1] + " * " + expressions[temp2] + ")"
+                        der = "(" + derivatives[temp1] + " * " + expressions[temp2] + " + " + expressions[temp1]\
+                              + " * " + derivatives[temp2] + ")"
+                    elif elem == '/':
+                        if expressions[temp1] == '0':
                             expr = '0'
                             der = '0'
                         else:
                             expr = "(" + expressions[temp1] + " / " + expressions[temp2] + ")"
-                            der = "(" + derivatives[temp1] + " * " + expressions[temp2] + " - " + expressions[temp1] + " * " + derivatives[temp2] + ")" \
+                            der = "(" + derivatives[temp1] + " * " + expressions[temp2] + " - " + expressions[temp1]\
+                                  + " * " + derivatives[temp2] + ")" \
                                 + " / (" + expressions[temp2] + " ** 2)"
-                    elif(elem == "**"):
+                    elif elem == "**":
                         expr = "(" + expressions[temp1] + " ** " + expressions[temp2] + ")"
                         tmp = list()
-                        if(derivatives[temp1] != '0'):
+                        if derivatives[temp1] != '0':
                             tmp.append(derivatives[temp1] + " / " + expressions[temp1] + " * " + expressions[temp2])
-                        if(derivatives[temp2] != '0'):
+                        if derivatives[temp2] != '0':
                             tmp.append(derivatives[temp2] + " * ln(" + expressions[temp1] + ")")
-                        if(len(tmp) == 0):
+                        if len(tmp) == 0:
                             der = '0'
                         else:
-                            der = "(" + expressions[temp1] + " ** " + expressions[temp2] + ") * (" + " + ".join(tmp) + ")"
-                        #der = "(" + expressions[temp1] + " ** " + expressions[temp2] + ") * (" + derivatives[temp1] + " / " + expressions[temp1] \
-                        #    + " * " + expressions[temp2] + " + " + derivatives[temp2] + " * ln(" + expressions[temp1] + "))"
+                            der = "(" + expressions[temp1] + " ** " + expressions[temp2] + ") * ("\
+                                  + " + ".join(tmp) + ")"
                 expressions.append(expr)
                 derivatives.append(der)
-                workStack.append(len(expressions) - 1)
+                work_stack.append(len(expressions) - 1)
 
-        #print("in:")
-        #print(instrstr)
-        #print("answer:")
-        #print(derivatives[len(derivatives) - 1])
         outfile.write(derivatives[len(derivatives) - 1])
         outfile.write('\n')
     infile.close()
